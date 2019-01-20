a0cba6dba75237a14eb2d97ea24a21c7dcdee91e
mtjin <tmddjs210@naver.com>
2019-01-20 19:32:21 +0900

    나인패치와 비트맵버튼생성 연습

---
+++ MyBitMap_button/app/src/main/java/com/example/mybitmap_button/BitmapButton.java
@@ -0,0 +1,42 @@
+package com.example.mybitmap_button;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.widget.AppCompatButton;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class BitmapButton extends AppCompatButton {
+    public BitmapButton(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public BitmapButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    private  void init(Context context){
+        setBackgroundResource(R.drawable.title_bitmap_button_normal);
+        float textSize = getResources().getDimension(R.dimen.text_size);
+        setTextSize(textSize);
+        setTextColor(Color.WHITE);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        switch (action){
+            case MotionEvent.ACTION_DOWN:
+                setBackgroundResource(R.drawable.title_bitmap_button_clicked);
+                break;
+            case MotionEvent.ACTION_UP:
+                setBackgroundResource(R.drawable.title_bitmap_button_normal);
+                break;
+        }
+        invalidate();
+
+        return true;
+    }
+}
