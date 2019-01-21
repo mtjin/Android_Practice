7e46961fa2d85c7bfc6e992aec60554bcde01aa6
mtjin <tmddjs210@naver.com>
2019-01-21 12:22:16 +0900

    Infate 연습

---
+++ MyLayoutInflater_practice/app/src/main/java/com/example/mylayoutinflater_practice/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.mylayoutinflater_practice;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+    FrameLayout container;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        container = findViewById(R.id.container);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.sub1, container, true);
+
+            }
+        });
+    }
+}
