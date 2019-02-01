d456aba92c98ba49a6b7246fc4a000ff3b7acfb3
seungeon <tmddjs210@naver.com>
2019-02-01 18:54:49 +0900

    3장 화면여러개만들기 요약정리연습

--- MySummary3/app/src/main/java/com/example/a82107/mysummary3/CommentWriteActivity.java
+++ MySummary3/app/src/main/java/com/example/a82107/mysummary3/CommentWriteActivity.java
@@ -1,13 +1,52 @@
 package com.example.a82107.mysummary3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 public class CommentWriteActivity extends AppCompatActivity {
+    RatingBar ratingBar;
+    EditText contentsInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment_write);
+
+        ratingBar = findViewById(R.id.ratingBar);
+        contentsInput = findViewById(R.id.contentsInput);
+
+        Button saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnToMain();
+            }
+        });
+
+        Intent intent = getIntent();
+        processIntent(intent);
+    }
+
+    public void processIntent(Intent intent){
+        if(intent != null){
+            float rating = intent.getFloatExtra("rating", 0.0f);
+            ratingBar.setRating(rating);
+        }
+
+    }
+
+    public void returnToMain(){
+        String contents = contentsInput.getText().toString();
+        Intent intent = new Intent();
+        intent.putExtra("contents", contents);
+
+        setResult(RESULT_OK, intent);
+
+        finish();
     }
 }
--- MySummary3/app/src/main/java/com/example/a82107/mysummary3/MainActivity.java
+++ MySummary3/app/src/main/java/com/example/a82107/mysummary3/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.a82107.mysummary3;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     RatingBar ratingBar;
+    TextView outputView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ratingBar = (RatingBar)findViewById(R.id.ratingBar);
+        outputView = findViewById(R.id.outputView);
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener((new View.OnClickListener() {
@@ -26,6 +31,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showCommentWriteActivity(){
+        float rating = ratingBar.getRating();
+        Intent intent = new Intent(getApplicationContext(), CommentWriteActivity.class);
+        intent.putExtra("rating",rating);
+        //응답을 받을거니깐 ForResult로함
+        startActivityForResult(intent, 101);
+    }
 
+    //응답받을떄 사용하는 메소드
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if(requestCode == 101){
+            if(intent != null){
+                String contents = intent.getStringExtra("contents");
+                outputView.setText(contents);
+            }
+        }
     }
 }
