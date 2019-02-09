b25c8787f61a43d625fac32b5da5042a7267c7b7
seungeon <tmddjs210@naver.com>
2019-02-09 16:17:54 +0900

    프래그먼트 공부, 프로그래먼트 변환 및 라이프사이클메소드 사용법

--- MyFragment1/app/src/main/java/com/example/a82107/myfragment1/MenuFragment.java
+++ MyFragment1/app/src/main/java/com/example/a82107/myfragment1/MenuFragment.java
@@ -14,6 +14,7 @@ public class MenuFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //프래그먼트 메뉴를 인플레이트해주고 컨테이너에 붙여달라는 뜻임
+        //false 부분은 프래그먼트가 액티비티위에 올라갔을때 동작을 하니깐 일단 바로 붙이지 않도록 false로하는거다
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_menu, container, false);
         return rootView;
     }
