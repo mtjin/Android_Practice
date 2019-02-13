47946128d32893b26a6011eaf84af11ade3568f9
seungeon <tmddjs210@naver.com>
2019-02-14 02:29:44 +0900

    바로가기 메뉴 만들기 실습(NavigationDrawer)

--- MyDrawer/app/src/main/java/com/example/a82107/mydrawer/MainActivity.java
+++ MyDrawer/app/src/main/java/com/example/a82107/mydrawer/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a82107.mydrawer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,25 +13,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+//내가 만든 인터페이스를 추가함(FragmentCallback)
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, FragmentCallback {
+
+    Fragment fragment1;
+    Fragment fragment2;
+    Fragment fragment3;
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //툴바가 xml 레이아웃으로 정의가 되면 그거를 이 화면에 설정할 때는 이런식으로 툴바를 다시 설정을 해줘야한다.
+        //mainxml에서 AppBarLayout이라고 하는걸로 액션바 부분을 잡아주게되면 기본적으로 이코드를 넣어야된다고 생각하면됨
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -39,7 +42,33 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        //바로가기 메뉴에서 메뉴가 하나 선택되면 이 리스너가 호출만듬.
         navigationView.setNavigationItemSelectedListener(this);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment1).commit();
+    }
+
+    @Override
+    public void onFragmentSelected(int position, Bundle bundle) {
+        Fragment curFragment = null;
+
+        //해당 포지션에 맞게 툴바 타이틀 이름 바꿔줌
+        if (position == 0) {
+            curFragment = fragment1;
+            toolbar.setTitle("첫번째 화면");
+        } else if (position == 1) {
+            curFragment = fragment2;
+            toolbar.setTitle("두번째 화면");
+        } else if (position == 2) {
+            curFragment = fragment3;
+            toolbar.setTitle("세번째 화면");
+        }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, curFragment).commit();
     }
 
     @Override
@@ -80,18 +109,16 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_0) {
+            Toast.makeText(this, "첫번째 메뉴선택됨", Toast.LENGTH_LONG).show();
+            //내가 만든 인터페이스 함수 호출
+            onFragmentSelected(0, null);
+        } else if (id == R.id.nav_1) {
+            Toast.makeText(this, "두번째 메뉴선택됨", Toast.LENGTH_LONG).show();
+            onFragmentSelected(1, null);
+        } else if (id == R.id.nav_2) {
+            Toast.makeText(this, "세번째 메뉴선택됨", Toast.LENGTH_LONG).show();
+            onFragmentSelected(2, null);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
