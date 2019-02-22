2fa821e3c682e9c5190c917734f9616327026321
seungeon <tmddjs210@naver.com>
2019-02-22 20:42:47 +0900

    소캣프로그래밍 실습

---
+++ MyServer/app/src/main/java/com/example/a82107/myserver/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.a82107.myserver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSocketServer();
+            }
+        });
+    }
+
+    public void startSocketServer(){
+        
+    }
+}
