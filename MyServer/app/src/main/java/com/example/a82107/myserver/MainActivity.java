9af485a47885d2ca24118487a00034b366c776ad
seungeon <tmddjs210@naver.com>
2019-02-23 23:32:29 +0900

    소캣 서버,클라이언트 실습

--- MyServer/app/src/main/java/com/example/a82107/myserver/MainActivity.java
+++ MyServer/app/src/main/java/com/example/a82107/myserver/MainActivity.java
@@ -2,9 +2,18 @@ package com.example.a82107.myserver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+//인터넷기능을 쓸려면 maifest에  <uses-permission android:name="android.permission.INTERNET"/> 추가해야함
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,12 +25,46 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startSocketServer();
+                ServerThread thread = new ServerThread();
+                thread.start();
             }
         });
     }
 
-    public void startSocketServer(){
-        
+    class ServerThread extends Thread {
+        @Override
+        public void run() {
+            //클라이언트가 이 포트번호로만 접속을 해야함
+            int port = 5001;
+            try {
+                ServerSocket server = new ServerSocket(port);
+                Log.d("ServerThread", "서버가 실행됨.");
+
+                while (true) {//클라이언트 연결 대기상태로 들어가게됨(대기를 하다가 클라이언트가 접속을 하게되면 소캣객체가 반환됨)
+                    //이 소캣객체를 통해서 클라이언트가 요청한 데이터를 받을 수 있다.
+                    Socket socket = server.accept();
+                    /*InetAddress clientHost = socket.getLocalAddress();
+                    int clientPort = socket.getPort();*/
+
+
+                    ObjectInputStream instream = new ObjectInputStream((socket.getInputStream()));
+                    Object input = instream.readObject();
+                    Log.d("ServerThread", "input : " + input);
+
+                    ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+                    outputStream.writeObject(input + " from server.");
+                    //write를 한후 버퍼에 남아있는게 있을수도 있으므로 flush를 해줌
+                    outputStream.flush();
+                    Log.d("ServerThread", "output 보냄.");
+
+                    //클라이언트 요청을 다 처리한 후 소캣을 끊어줬음
+                    socket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- MySocket/app/src/main/java/com/example/a82107/mysocket/MainActivity.java
+++ MySocket/app/src/main/java/com/example/a82107/mysocket/MainActivity.java
@@ -2,12 +2,65 @@ package com.example.a82107.mysocket;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.SocketHandler;
+
+//MyServer클래스와 통신(클라이언트역할함 이프로젝트는)
+//인터넷기능을 쓸려면 manifest에  <uses-permission android:name="android.permission.INTERNET"/> 추가해야함
+//UI까지 손댈려면 핸들러를 사용해야한다.
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClientThread thread = new ClientThread();
+                thread.start();
+            }
+        });
+    }
+
+    class ClientThread extends  Thread{
+        public  void run(){
+            //어떤 컴퓨터에 접속할지 IP지정(localhost라 하면 자기 자신을 가리킴)
+            String host = "localhost";
+            //Server와 동일하게 포트 설정해줌
+            int port = 5001;
+
+            try {
+                Socket socket = new Socket(host, port);
+                //얜 순서가 서버와 다르게되있다. 먼저 보내고 받는 순으로 되있음
+
+                //보내기위한 통로를 만듬
+                ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
+                outStream.writeObject("안녕하세요!");
+                //write를 쓰면 flush를 해주는거 기억해라
+                outStream.flush();
+                Log.d("ClientThread", "서버로 보냄.");
+
+                //받기위한통로
+                ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
+                Object input = inStream.readObject();
+                Log.d("ClientThread", "받은 데이터 : " + input);
+
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
