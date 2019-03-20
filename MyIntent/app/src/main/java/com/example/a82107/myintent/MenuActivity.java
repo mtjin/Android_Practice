9b6f112d0820ae23836c8d37f2315550550fe0b2
seungeon <tmddjs210@naver.com>
2019-03-20 22:19:35 +0900

    인텐트 사용법 실습

--- MyIntent/app/src/main/java/com/example/a82107/myintent/MenuActivity.java
+++ MyIntent/app/src/main/java/com/example/a82107/myintent/MenuActivity.java
@@ -22,6 +22,7 @@ public class MenuActivity extends AppCompatActivity {
                 //해시처럼 키와  벨류값을 설정 (이 액티비티를 불러준 곳에 데이터를 전달할 용도)
                 intent.putExtra("name", "mike");
                 //Result_OK는 일종의신호이다. 이상이없는 신호라는걸 의미함
+                //이 액티비티가 종료되기전에 이 메소드를 호출함으로써 메인액티비티에 응답을 보낼 수 있다.
                 setResult(Activity.RESULT_OK, intent);
 
                 finish();
