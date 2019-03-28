cc3ac96f4b2001de566b4b05e50dd0ac329cec3e
seungeon <tmddjs210@naver.com>
2019-03-28 12:20:30 +0900

    인터넷 연결상태 확인하는법

---
+++ MyConnectStatus2/app/src/main/java/com/example/myconnectstatus/NetworkStatus.java
@@ -0,0 +1,26 @@
+package com.example.myconnectstatus;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkStatus {
+    public static final int TYPE_WIFI = 1;
+    public static final int TYPE_MOBILE = 2;
+    public static final int TYPE_NOT_CONNECTED = 3;
+
+    public static int getConnectivityStatus(Context context){
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        if(networkInfo != null){
+            int type = networkInfo.getType();
+            if(type == ConnectivityManager.TYPE_MOBILE){//쓰리지나 LTE로 연결된것
+                return TYPE_MOBILE;
+            }else if(type == ConnectivityManager.TYPE_WIFI){
+                return TYPE_WIFI;
+            }
+        }
+        return TYPE_NOT_CONNECTED;
+    }
+}
